eaedd9625a3e616cc6c5dc1062c00ce5a351370d
McChesterson <114367559+McChesterson@users.noreply.github.com>
2024-04-24 23:48:45 -0700

    added project and basic images

---
+++ AlchemistsArrow/Alchemists Arrow/Assets/Scripts/PlayerJoinManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.UI;
+using System.ComponentModel;
+
+public class PlayerJoinManager : NetworkBehaviour
+{
+    public Image[] joinIcons = { null, null, null, null };
+    public Color player1JoinedColor;
+    public Color player2JoinedColor;
+    public Color player3JoinedColor;
+    public Color player4JoinedColor;
+    int currentAvailibleSpot;
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        Initialize();
+    }
+    private void Initialize()
+    {
+        currentAvailibleSpot = 0;
+    }
+
+}
